855ddc94f752411d33d2201175c3cd5ecae5e457
Xℹ Ruoyao <xry111@mengyan1223.wang>
2020-10-12 22:13:36 +0800

    try multiple c++ std version

--- testdata/compile.sh
+++ testdata/compile.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 g++ -DONLINE_JUDGE -fno-tree-ch -O2 -Wall -std=c++14 -pipe -lm $1 -o prog
+
+if [ "$?" = "0" ]; then
+	exit 0
+fi
+
+g++ -DONLINE_JUDGE -fno-tree-ch -O2 -Wall -std=c++11 -pipe -lm $1 -o prog
+
+if [ "$?" = "0" ]; then
+	exit 0
+fi
+
+g++ -DONLINE_JUDGE -fno-tree-ch -O2 -Wall -std=c++03 -pipe -lm $1 -o prog
